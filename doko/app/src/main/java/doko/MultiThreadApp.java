62660476e3c8ca3ab788903367d58c98943be049
Twelfthonehone <honehone.41games@gmail.com>
2025-10-20 15:00:03 +0900

    using virtual thread

--- doko/app/src/main/java/doko/MultiThreadApp.java
+++ doko/app/src/main/java/doko/MultiThreadApp.java
@@ -4,7 +4,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 
 public abstract class MultiThreadApp {
-    private static final ExecutorService GLOBAL_EXECUTER = Executors.newCachedThreadPool();
+    private static final ExecutorService GLOBAL_EXECUTER = Executors.newVirtualThreadPerTaskExecutor();
 
     protected static ExecutorService Executer() {
         return GLOBAL_EXECUTER;
